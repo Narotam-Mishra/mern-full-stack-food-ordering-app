c97d65ad9921b9c7ea03d7e87b09fe491fd8bced
Narotam-Mishra <narotam21jan@gmail.com>
2024-09-10 14:40:18 +0530

    full-stack-food-ordering_app_navbar-design_inprogress_1.3

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,8 +1,28 @@
+import { useState } from 'react'
 import "./Navbar.css"
+import { assets } from '../../assets/assets'
 
 const Navbar = () => {
+  const [menu, setMenu] = useState("menu");
+
   return (
-    <div className='navbar'></div>
+    <div className='navbar'>
+      <img src={assets.logo} alt="app_logo" className="logo" />
+      <ul className="navbar-menu">
+        <li onClick={() => setMenu("home")} className={menu === "home" ? "active" : ""}>home</li>
+        <li onClick={() => setMenu("menu")} className={menu === "menu" ? "active" : ""}>menu</li>
+        <li onClick={() => setMenu("mobile-app")} className={menu === "mobile-app" ? "active" : ""}>mobile-app</li>
+        <li onClick={() => setMenu("contact-us")} className={menu === "contact-us" ? "active" : ""}>contact us</li>
+      </ul>
+      <div className="navbar-right">
+        <img src={assets.search_icon} alt="search_icon" />
+        <div className="navbar-search-icon">
+          <img src={assets.basket_icon} alt="" />
+          <div className="dot"></div>
+        </div>
+        <button>sign in</button>
+      </div>
+    </div>
   )
 }
 
