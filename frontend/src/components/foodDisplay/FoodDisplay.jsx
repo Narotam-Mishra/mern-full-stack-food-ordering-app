f98de2bb9386077c8f6c5507230f468abdbc0808
Narotam-Mishra <narotam21jan@gmail.com>
2024-09-15 23:05:24 +0530

    full-stack-food-ordering_app_food_Item_com_added_1.10

--- frontend/src/components/foodDisplay/FoodDisplay.jsx
+++ frontend/src/components/foodDisplay/FoodDisplay.jsx
@@ -2,8 +2,10 @@
 import { useContext } from "react"
 import "./FoodDisplay.css"
 import { StoreContext } from "../../context/StoreContext"
+import FoodItem from "../foodItem/FoodItem";
 
 const FoodDisplay = ({ category }) => {
+  
   const { food_list } = useContext(StoreContext);
 
   return (
@@ -12,7 +14,16 @@ const FoodDisplay = ({ category }) => {
         <div className="food-display-list">
             {
                 food_list.map((item, index) => {
-                    return
+                  return (
+                    <FoodItem
+                      key={index}
+                      id={item._id}
+                      name={item.name}
+                      description={item.description}
+                      price={item.price}
+                      image={item.image}
+                    />
+                  );
                 })
             }
         </div>
