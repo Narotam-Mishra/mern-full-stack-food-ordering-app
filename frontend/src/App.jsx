ff492d89977140d4d0f6f94d7bc7db4a2d4233ca
Narotam-Mishra <narotam21jan@gmail.com>
2024-09-19 22:51:09 +0530

    full-stack-food-ordering_app_loginpopup_added_1.15

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,12 +4,17 @@ import Home from "./pages/home/Home"
 import Cart from "./pages/cart/Cart"
 import PlaceOrder from "./pages/PlaceOrder/PlaceOrder"
 import Footer from "./components/footer/Footer"
+import { useState } from "react"
+import LoginPopup from "./components/loginPopup/LoginPopup"
 
 const App = () => {
+  const [showLogin, setShowLogin] = useState(false);
+
   return (
     <>
+      { showLogin ? <LoginPopup setShowLogin={setShowLogin} /> : <></> }
       <div className="app">
-        <Navbar />
+        <Navbar setShowLogin={setShowLogin} />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
