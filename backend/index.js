e31eb507c020e821441c78f74c9a6afcb9884f7a
Narotam-Mishra <narotam21jan@gmail.com>
2024-09-23 22:45:29 +0530

    full-stack-food-ordering_app_backend_files_setup_1.23

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,26 @@
 
-console.log("Food Ordering App Backend...");
+import expess from "express"
+import cors from "cors"
+import express from "express";
+import evar from 'dotenv';
+
+// configure environment variables
+evar.config();
+
+// app config
+const app = express()
+
+const portNo = process.env.PORT || 5874;
+
+// middleware
+app.use(expess.json())
+app.use(cors())
+
+app.get("/", (req, res) => {
+    res.send("API Working")
+})
+
+app.listen(portNo, () => {
+    console.log(`Server started on port: ${portNo}`);
+})
+
