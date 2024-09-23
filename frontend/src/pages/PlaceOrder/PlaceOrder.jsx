8ba9eefcd27c005454256210940b4df91c01197a
Narotam-Mishra <narotam21jan@gmail.com>
2024-09-23 13:49:36 +0530

    full-stack-food-ordering_app_place_order_page_completed_1.21

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import "./PlaceOrder.css"
+import "./PlaceOrder.css";
 import { StoreContext } from "../../context/StoreContext";
 
 const PlaceOrder = () => {
@@ -36,21 +36,21 @@ const PlaceOrder = () => {
             <hr />
             <div className="cart-total-details">
               <p>Delivery Fee</p>
-              <p>${2}</p>
+              <p>${getTotalCartAmount() === 0 ? 0 : 2}</p>
             </div>
             <hr />
             <div className="cart-total-details">
               <b>Total</b>
-              <b>${getTotalCartAmount() + 2}</b>
+              <b>
+                ${getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 2}
+              </b>
             </div>
           </div>
-          <button>
-            PROCEED TO PAYMENT
-          </button>
+          <button>PROCEED TO PAYMENT</button>
         </div>
       </div>
     </form>
   );
-}
+};
 
-export default PlaceOrder
+export default PlaceOrder;
