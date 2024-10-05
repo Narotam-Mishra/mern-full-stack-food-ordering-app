096a3962793d6074fa76cc488be58462879ce9c9
Narotam-Mishra <narotam21jan@gmail.com>
2024-10-05 23:34:28 +0530

    full-stack-food-ordering_app_cart_apis_integrated_with_frontend_1.40

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -13,16 +13,22 @@ const StoreContextProvider = (props) => {
     const [token, setToken] = useState("")
     const [food_list, setFoodList] = useState([])
 
-    const addToCart = (itemId) => {
+    const addToCart = async (itemId) => {
         if(!cartItems[itemId]){
             setCartItems((prev) => ({ ...prev, [itemId]:1 }));  
         }else{
             setCartItems((prev) => ({ ...prev, [itemId]:prev[itemId]+1 }));
         }
+        if(token){
+            await axios.post(backendUrl + "/api/cart/add", { itemId }, {headers: {token}});
+        }
     }
 
-    const removeFromCart = (itemId) => {
+    const removeFromCart = async (itemId) => {
         setCartItems((prev) => ({ ...prev, [itemId]:prev[itemId] - 1 }));
+        if(token){
+            await axios.post(backendUrl + "/api/cart/remove", { itemId }, {headers: {token}});
+        }
     }
 
     // useEffect(() => {
@@ -46,11 +52,17 @@ const StoreContextProvider = (props) => {
         setFoodList(response.data.data);
     }
 
+    const loadCartData = async (token) => {
+        const response = await axios.post(backendUrl + "/api/cart/get", {}, {headers: {token}});
+        setCartItems(response.data.cartData);
+    }
+
     useEffect(() => {
         async function loadData() {
             await fetchFoodList();
             if(localStorage.getItem("JWT_Token")){
                 setToken(localStorage.getItem("JWT_Token"));
+                await loadCartData(localStorage.getItem("JWT_Token"));
             }
         }
         loadData();
